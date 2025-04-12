eb68f00863e10f1806ec2931d86811cec79888f9
Yousuf-Gazi <gaziyousufridoy@gmail.com>
2025-04-12 09:22:21 +0600

    feat: add core game components and update main logic
    - Added new modules: asteroid.py, asteroidfield.py, circleshape.py, player.py, shot.py
    - Modified constants.py and main.py to integrate new game objects
    - Included accidental "\\" file (consider removing if unintentional)

--- main.py
+++ main.py
@@ -1,18 +1,58 @@
 import pygame
+import sys
 
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        # limit the framerate to 60 FPS
+        time_passed = clock.tick(60)
+        dt = time_passed / 1000
+
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game Over!")
+                sys.exit()
+            
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    asteroid.split()
+                    shot.kill()
+
         screen.fill(color="black")
+
+        for drawable_thing in drawable:
+            drawable_thing.draw(screen)
+
         pygame.display.flip()
+
 
 
 if __name__ == "__main__":
